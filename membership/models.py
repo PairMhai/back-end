90015c3c44e87c80a4787ba448da8a8bda5342b9
Piromsurang Rungserichai <gift.rungserichai@gmail.com>
2017-09-01 14:21:16 +0700

    example of membership models.py

---
+++ membership/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    telephone = models.CharField(max_length=13)
+    address = models.TextField()
+    classes = models.ForeignKey(
+        'Class',
+        on_delete = models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Class(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField() 
