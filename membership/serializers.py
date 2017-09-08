0ed006947eef6551ddd857ca51ac38b87ed70e6f
kamontat <kamontat_c@hotmail.com>
2017-09-08 22:09:24 +0700

    add class serializer

--- membership/serializers.py
+++ membership/serializers.py
@@ -1,9 +1,13 @@
-from membership.models import Customer
+from membership.models import Customer, Class
 from rest_framework import serializers
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ('id', 'firstname', 'lastname',
-                  'email', 'telephone', 'address',
-                  'classes', 'created_at', 'updated_at')
+                  'email', 'classes')
+
+class ClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Class
+        fields = ('id', 'name', 'price', 'description')
