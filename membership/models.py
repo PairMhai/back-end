ce09c2302a4619b97b2ed418b811858b61967682
kamontat <kamontat_c@hotmail.com>
2017-09-13 23:25:49 +0700

    add test of membership

--- membership/models.py
+++ membership/models.py
@@ -7,12 +7,12 @@ class User(AbstractUser):
     """customer information v1"""
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
-    telephone = models.CharField(max_length=13, default="")
+    telephone = models.CharField(max_length=13, default="0XX-XXX-XXXX")
     address = models.TextField(default="")
     date_of_birth = models.DateField(null=True)
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
-        return self.firstname + " " + self.lastname
+        return self.username + ": " + self.firstname + " " + self.lastname
 
 # merge django auth user to customer
 class Customer(models.Model):
@@ -26,7 +26,22 @@ class Customer(models.Model):
         on_delete=models.CASCADE
     )
     def __str__(self):
-        return self.user.firstname + " " + self.user.lastname
+        return self.user
+
+# merge django auth user to customer
+class Admin(models.Model):
+    """customer information v1"""
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    user.is_staff = True
+    classes = models.ForeignKey(
+        'Class',
+        on_delete=models.CASCADE
+    )
+    def __str__(self):
+        return self.user
 
 class Class(models.Model):
     """membership class v1"""
