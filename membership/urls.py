48b17ea3c1f2281985bf9694cfd4d6180304fb0f
kamontat <kamontat_c@hotmail.com>
2017-09-14 22:59:30 +0700

    add url name for testing

--- membership/urls.py
+++ membership/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from membership.views import CustomerAction, CustomerDetail, ClassDetail
 
 urlpatterns = [
-    url(r'^$', CustomerAction.as_view()),
-    url(r'^id/(?P<pk>[0-9]+)$', CustomerDetail.as_view()),
-    url(r'^class/(?P<pk>[0-9]+)$', ClassDetail.as_view()),
+    url(r'^$', CustomerAction.as_view(), name="membership"),
+    url(r'^id/(?P<pk>[0-9]+)$', CustomerDetail.as_view(), name="membership-detail"),
+    url(r'^class/(?P<pk>[0-9]+)$', ClassDetail.as_view(), name="membership-class"),
 ]
