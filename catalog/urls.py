99acdc775f97824059a04993a08721afe2fb887e
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2017-09-14 18:39:01 +0700

    fixes serializer class and url path (#5)

---
+++ catalog/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from catalog.views import *
+
+urlpatterns = [
+    url(r'^materials$', MaterialList.as_view()),
+    url(r'^material/(?P<pk>[0-9]+)$', MaterialDetail.as_view()),
+    url(r'^designs$', DesignList.as_view()),
+    url(r'^design/(?P<pk>[0-9]+)$', DesignDetail.as_view()),
+    url(r'^patterns$', PatternList.as_view()),
+    url(r'^pattern/(?P<pk>[0-9]+)$', PatternDetail.as_view()),
+]
