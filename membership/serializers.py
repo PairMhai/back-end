06136f17bd8ec3c5e9b04447868e5a2fb0667e3f
kamontat <kamontat_c@hotmail.com>
2017-09-14 22:10:49 +0700

    update serializer and view

--- membership/serializers.py
+++ membership/serializers.py
@@ -1,35 +1,51 @@
 from membership.models import User, Customer, Class
 from rest_framework import serializers
 
-# from test
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
 
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'email')
 
+
 class FullUserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'address', 'date_of_birth', 'telephone')
+        fields = ('id', 'username', 'first_name', 'last_name',
+                  'email', 'address', 'date_of_birth', 'telephone')
+
 
 class CustomerSerializer(serializers.ModelSerializer):
     user = UserSerializer()
+
     class Meta:
         model = Customer
-        fields = ('id', 'user', 'classes')
+        fields = ('id', 'user')
 
     def create(self, validated_data):
-        user_data = validated_data.pop('user') # get user json
-        pp.pprint(user_data)
-        user = User.objects.create(**user_data) # create user
-        # fix class be id 1
-        customer = Customer.objects.create(user=user, classes="1") # create customer
+        user_data = ""
+        if (self.context['request'].method == "POST"):
+            serializer = FullUserSerializer(
+                data=self.context['request'].data.pop('user')
+            )
+            if (serializer.is_valid()):
+                user_data = serializer.validated_data
+            else:
+                print("SERIALIZER ERRORS: ", serializer.errors)
+                return None
+        else:
+            user_data = validated_data.pop('user')  # get user json
+        user = User.objects.create(**user_data)  # create user
+        class_bronze = Class.objects.get(id=1)  # get bronze class by default
+        customer = Customer.objects.create(
+            user=user, classes=class_bronze)  # create customer
         return customer
 
+
 class ClassSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Class
         fields = ('id', 'name', 'price', 'description')
--- membership/views.py
+++ membership/views.py
@@ -2,26 +2,27 @@ from membership.models import Customer, Class
 from membership.serializers import CustomerSerializer, ClassSerializer
 # from django.views.decorators.csrf import csrf_exempt
 
+# from test
+import pprint
+
 from rest_framework import generics
 
-#Create your views here.
+
 class CustomerAction(generics.ListCreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
-    def perform_create(self, serializer):
-        print("self: " + str(self.request))
-        print("serializer: " + str(serializer))
-        serializer.save(self.request.user)
 
 class CustomerDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
+
 class ClassDetail(generics.RetrieveAPIView):
     queryset = Class.objects.all()
     serializer_class = ClassSerializer
     # example of custom response json format
+
     def retrieve(self, request, *args, **kwargs):
         serializer = self.get_serializer(self.get_object())
-        return Response({"successful" : True, "data": serializer.data})
+        return Response({"successful": True, "data": serializer.data})
