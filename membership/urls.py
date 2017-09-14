f54e387b0867ad54cecff53642ca9b3d7c173c38
kamontat <kamontat_c@hotmail.com>
2017-09-14 23:41:02 +0700

    remove id

--- membership/urls.py
+++ membership/urls.py
@@ -3,6 +3,6 @@ from membership.views import CustomerAction, CustomerDetail, ClassDetail
 
 urlpatterns = [
     url(r'^$', CustomerAction.as_view(), name="membership"),
-    url(r'^id/(?P<pk>[0-9]+)$', CustomerDetail.as_view(), name="membership-detail"),
+    url(r'^(?P<pk>[0-9]+)$', CustomerDetail.as_view(), name="membership-detail"),
     url(r'^class/(?P<pk>[0-9]+)$', ClassDetail.as_view(), name="membership-class"),
 ]
