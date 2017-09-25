dff542d04dde851dcf6b858a10cf02571e39d71d
kamontat <kamontat_c@hotmail.com>
2017-09-25 23:36:35 +0700

    add rest-auth url

--- membership/urls.py
+++ membership/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from membership.views import CustomerAction, CustomerDetail, ClassDetail
 
 urlpatterns = [
-    url(r'^$', CustomerAction.as_view(), name="membership"),
-    url(r'^(?P<pk>[0-9]+)$', CustomerDetail.as_view(), name="membership-detail"),
-    url(r'^class/(?P<pk>[0-9]+)$', ClassDetail.as_view(), name="membership-class"),
+    url(r'^/', include('rest_auth.urls')),
+    url(r'^register/', include('rest_auth.registration.urls'))
+    # url(r'^$', CustomerAction.as_view(), name="membership"),
+    # url(r'^(?P<pk>[0-9]+)$', CustomerDetail.as_view(), name="membership-detail"),
+    # url(r'^class/(?P<pk>[0-9]+)$', ClassDetail.as_view(), name="membership-class"),
 ]
