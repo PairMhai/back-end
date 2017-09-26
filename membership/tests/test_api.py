1c7ab8145708029dffd078263eeefd3c5dd55f33
Piromsurang Rungserichai <gift.rungserichai@gmail.com>
2017-09-26 17:29:16 +0700

    test different passwords for registration

--- membership/tests/test_api.py
+++ membership/tests/test_api.py
@@ -58,11 +58,24 @@ class ViewTestCase(TestCase):
             "password2": "asdf123fdssa"
         }
 
+        number = str(round(uniform(0, 10000), 5))
+        self.bad_user_with_diff_passwords = {
+            "user": {
+                "username": "good-user" + number,
+                "first_name": "good" + number,
+                "last_name": "user",
+                "email": "good" + number + "@user.com"
+            },
+            "classes": randrange(1, 6),
+            "password1": "asdf123fdssa",
+            "password2": "asdf123fdss1"
+        }
+
     def test_api_bad_request_no_first_last_email(self):
         """firstname lastname and email are required to filled."""
         self.client.force_authenticate(user=self.admin)
         response = self.client.post(
-            reverse('membership'),
+            reverse('rest_register'),
             self.bad_user,
             format="json"
         )
@@ -84,7 +97,7 @@ class ViewTestCase(TestCase):
         """if client don't specify class, use default bronze class."""
         self.client.force_authenticate(user=self.admin)
         response = self.client.post(
-            reverse('membership'),
+            reverse('rest_register'),
             self.good_user,
             format="json"
         )
@@ -95,9 +108,21 @@ class ViewTestCase(TestCase):
         """create customer randomly."""
         self.client.force_authenticate(user=self.admin)
         response = self.client.post(
-            reverse('membership'),
+            reverse('rest_register'),
             self.good_user_with_classes,
             format="json"
         )
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.client.logout()
+
+    def test_api_dismatch_password(self):
+        """wrong password cannot login"""
+        self.client.force_authenticate(user=self.admin)
+        response = self.client.post(
+            reverse('rest_register'),
+            self.bad_user_with_diff_passwords,
+            format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.client.logout()
--- membership/urls.py
+++ membership/urls.py
@@ -3,7 +3,7 @@ from membership.views import CustomerAction, CustomerDetail, ClassDetail
 
 urlpatterns = [
     url(r'^', include('rest_auth.urls')),
-    url(r'^register/', include('rest_auth.registration.urls'))
+    url(r'^register/', include('rest_auth.registration.urls')),
     # url(r'^$', CustomerAction.as_view(), name="membership"),
     # url(r'^(?P<pk>[0-9]+)$', CustomerDetail.as_view(), name="membership-detail"),
     # url(r'^class/(?P<pk>[0-9]+)$', ClassDetail.as_view(), name="membership-class"),
