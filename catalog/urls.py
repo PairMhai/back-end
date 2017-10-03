9bff9066da93ef12a0ac02af5f6d039b9ce71b3f
kamontat <kamontat_c@hotmail.com>
2017-10-03 20:23:55 +0700

    add name of path in catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from catalog.views import *
 
 urlpatterns = [
-    url(r'^materials$', MaterialList.as_view()),
-    url(r'^material/(?P<pk>[0-9]+)$', MaterialDetail.as_view()),
-    url(r'^designs$', DesignList.as_view()),
-    url(r'^design/(?P<pk>[0-9]+)$', DesignDetail.as_view()),
-    url(r'^promotions$', PromotionList.as_view()),
-    url(r'^promotion/(?P<pk>[0-9]+)$', PromotionDetail.as_view()),
+    url(r'^materials$', MaterialList.as_view(), name='material-list'),
+    url(r'^material/(?P<pk>[0-9]+)$', MaterialDetail.as_view(), name='material-detail'),
+    url(r'^designs$', DesignList.as_view(), name='design-list'),
+    url(r'^design/(?P<pk>[0-9]+)$', DesignDetail.as_view(), name='design-detail'),
+    url(r'^promotions$', PromotionList.as_view(), name='promotion-list'),
+    url(r'^promotion/(?P<pk>[0-9]+)$', PromotionDetail.as_view(), name='promotion-detail'),
 ]
