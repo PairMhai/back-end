2b8038a40dc0db549b065ff5c06788f15451dcde
kamontat <kamontat_c@hotmail.com>
2017-10-03 19:34:21 +0700

    update comment api

--- comment/views.py
+++ comment/views.py
@@ -5,10 +5,6 @@ from comment.serializers import CommentSerializer
 from rest_framework import generics
 
 #Create your views here.
-class CommentCreator(generics.CreateAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-
-class CommentList(generics.ListAPIView):
+class CommentListAndCreateView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
