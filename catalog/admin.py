74229b86bacf59085c962f54106026d8c5b323c1
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2017-10-04 18:43:24 +0700

    add product when admin add design or material

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,14 +4,23 @@ from .models import Design, Material, Product, Promotion, Image
 class DesignAdmin(admin.ModelAdmin):
     class Meta:
         model = Design
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        Product.objects.create(material=None, design=Design.objects.get(pk=obj.id)).save()
 
 class MaterialAdmin(admin.ModelAdmin):
     class Meta:
         model = Material
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        Product.objects.create(material=Material.objects.get(pk=obj.id), design=None).save()
 
 class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ('design', 'material')
     class Meta:
         model = Product
+    def has_add_permission(self, request):
+        return False
 
 class PromotionAdmin(admin.ModelAdmin):
     class Meta:
