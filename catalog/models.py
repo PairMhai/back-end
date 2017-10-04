5b2f6521cc83b3ae062717d766d742bd79e20c3a
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2017-10-04 18:43:47 +0700

    update catalog models

--- catalog/models.py
+++ catalog/models.py
@@ -13,11 +13,14 @@ from django.db import models
 #         return self.name
 
 # Create your models here.
+
+
 class Design(models.Model):
     """design product v1"""
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=150)
-    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)  # max: 999,999.99
+    price = models.DecimalField(
+        max_digits=8, decimal_places=2, default=0.00)  # max: 999,999.99
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     material = models.ForeignKey(
@@ -28,13 +31,17 @@ class Design(models.Model):
     def __str__(self):
         return self.name
 
+    def get_product_id(self):
+        return Product.objects.get(design=self).id
+
 
 class Material(models.Model):
     """material product v1"""
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)  # description of material
     quantity = models.IntegerField(default=0)  # stock
-    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)  # max: 999,999.99
+    price = models.DecimalField(
+        max_digits=8, decimal_places=2, default=0.00)  # max: 999,999.99
     color = models.CharField(max_length=50)  # material color
     image_name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -43,6 +50,8 @@ class Material(models.Model):
     def __str__(self):
         return self.name
 
+    def get_product_id(self):
+        return Product.objects.get(material=self).id
 
 class Image(models.Model):
     """image of the material v1"""
@@ -62,22 +71,28 @@ class Image(models.Model):
 class Product(models.Model):
     design = models.ForeignKey(
         'Design',
+        related_name='product',
         null=True,
         on_delete=models.CASCADE
     )
     material = models.ForeignKey(
         'Material',
+        related_name='product',
         null=True,
         on_delete=models.CASCADE
     )
 
+    def __str__(self):
+        return "Design {} object".format(self.design.id) if self.material == None else "Material {} object".format(self.material.id)
 
-# move to cycle 3
+
 class Promotion(models.Model):
+    # move to cycle 3
     name = models.CharField(max_length=100)
     image_name = models.CharField(max_length=100)
     description = models.CharField(max_length=150)
-    discount = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+    discount = models.DecimalField(
+        max_digits=8, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     product = models.ForeignKey(
