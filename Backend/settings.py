8bace1bc7a1cf0c5cfb0b9ffa47721ba96952477
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2017-10-22 22:02:31 +0700

    🔖 Dump version 0.12.0
    add new catalog and update discount logic

--- Backend/settings.py
+++ Backend/settings.py
@@ -25,7 +25,7 @@ AdminSite.index_title = 'Administration Page'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-VERSION = "0.11.0"
+VERSION = "0.12.0"
 
 FIXTURE_DIRS = (
     BASE_DIR + '/Backend/fixtures/',
