d9aaed86875b06d6f5ad3849f099aaf71d481c0d
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2017-10-27 15:38:38 +0700

    ✨ new utils script
    add new feature and update document

--- utils.sh
+++ utils.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# setup project
+if [[ $1 == "setup" ]]; then
+    ! command -v conda &>/dev/null && echo "conda required to setup project!" && exit 1
+
+    echo ">> install conda dependencies."
+    conda create --name pairmhai --file requirements_conda.txt
+
+    echo ">> install dependencies."
+    pip install -r requirements.txt
+
+    exit 0
+fi
+
 # if conda available
 if command -v conda &>/dev/null; then
     conda info | grep pairmhai &>/dev/null ||\
@@ -97,7 +110,12 @@ elif [[ $1 == "m" ]]; then
     $COMMAND manage.py migrate $setting
 elif [[ $1 == "s" ]]; then
     setting=$(get_setting $2)
-    $COMMAND manage.py runserver $setting
+    if setting=$(get_setting $2); then
+        port="$3"
+    else
+        port="$2"
+    fi
+    $COMMAND manage.py runserver $setting $port
 elif [[ $1 == "c" ]]; then
     setting=$(get_setting $2)
     # cause error
@@ -152,36 +170,60 @@ elif [[ $1 == "t-ci" ]]; then
 elif [[ $1 == "reset-database" || $1 == "reset" || $1 == "r" ]]; then
     rm -rf db.sqlite3
     echo "remove database."
-elif [[ $1 == "clear-test-result" || $1 == "clear-test" || $1 == "ctr" ]]; then
+elif [[ $1 == "clear-test-result" || $1 == "clear-test" || $1 == "delete" ]]; then
     rm -rf ./test-reports/*
     echo "remove test-reports."
 else
     echo "
-Description: This is python utilities with django (To use this you must follow install helper in README.md)
-Global parameter: second parameter you can pass environment to the script (default=develop)
-                  and if you pass next parameter will shift automatically in script (don't worry)
+Description:
+    This is python utilities with django (To use this you must follow install helper in README.md)
+
+Global parameter:
+    'First Parameter' you can pass environment to the script (default=develop)
+    and if you pass next parameter will shift automatically in script (don't worry)
     - d | dev | develop
     - s | stage | staging
     - p | prod | production
-HELP Command:
-    1.  l    - load all fixture (test data)
-               - @params 1 - (optional) fixture name (without init_*)
-    2.  e    - dump currently database to file-name (if no file-name print as stout)
-               - @params 1 - models to export
-               - @params 2 - (optional) file name
-    3.  mm   - make migrations of new models
-    4.  m    - migrate database
-    5.  s    - run server
-    6.  h    - heroku short command
-               1. d - deploy code to heroku (@deprecated - pull to master for update production automatically)
-                      - @params 1 - (optional) branch to deploy (default is current branch)
-               2. l - logs all action in heroku container
-    7.  t    - test all testcase
-               - @params 1 - (optional) module.testcase.method is allow to spectify test
-    8.  t-ci - test all testcase with full version of debug print
-    9.  c    - check database problem
-    10  co   - collect static file
-    11. r    - remove currently database
-    12. ctr  - clear test-report
+
+Feature:
+1. support multiple command separate by "," like '. l,mm,m develop'
+
+Help Command:
+    # Setting
+        1. setup   - setup project after you download new project down.
+
+    # Develop
+        1. s       - run server (default port 8000)
+                     - @params 1 - (optional) port number
+
+    # Deploy
+        1. h       - heroku short command
+                     1. d - deploy code to heroku (@deprecated - pull to master for update production automatically)
+                          - @params 1 - (optional) branch to deploy (default is current branch)
+                     2. l - logs all action in heroku container
+        2. co      - collect static file
+
+    # Database
+        1. c       - check database problem
+        2. mm      - make migrations of new models
+        3. m       - migrate database
+        4. l       - load all fixture (test data)
+                     - @params 1 - (optional) fixture name (without init_*)
+        5. e       - dump currently database to file-name (if no file-name print as 'stout')
+                     - @params 1 - models to export
+                     - @params 2 - (optional) file name
+        6. r       - remove currently database
+
+    # Testing
+        1. t       - test all testcase
+                     - @params 1 - (optional) module.testcase.method is allow to spectify test
+        2. t-ci    - test all testcase with full debug printing
+        3. delete  - clear test-report
+
+Example Usage:
+1. './utils.sh s production 1234' - run server production on port 1234
+2. './utils.sh h d' - deploy current branch to heroku
+3. './utils.sh t membership.tests.test_login' - test all testcase in 'test_login' file
+4. './utils.sh r,m,l production' - remove current database -> migrate new -> load fixture (all done by production environment)
     "
 fi
