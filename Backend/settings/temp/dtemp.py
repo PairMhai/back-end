fe180c343319345015f36467288cb5a7dab95406
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2017-11-09 22:01:17 +0700

    :loud_sound: Add change log and temp files

---
+++ Backend/settings/temp/dtemp.py
@@ -0,0 +1,28 @@
+
+ACCOUNT_EMAIL_VERIFICATION = 'optional'
+# Email validation by gmail
+EMAIL_HOST_USER = 'pairmhai.wsp@gmail.com'
+EMAIL_HOST_PASSWORD = 'PWL-XA2-Rfy-r5b'
+# This did the trick
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+DEBUG = True
+
+TEST_OUTPUT_FILE_NAME = 'TEST-OUTPUT-DEVELOP-' + str(round(time.time())) + '.xml'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
