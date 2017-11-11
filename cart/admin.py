045f820ed82cfd15c5db68591f197caaafe8edb7
Piromsurang Rungserichai <gift.rungserichai@gmail.com>
2017-11-11 21:22:33 +0700

    able to show order info in admin order using inlines

--- cart/admin.py
+++ cart/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Order, OrderInfo, Transportation
 
+class OrderInfoInline(admin.TabularInline):
+    model = OrderInfo
+    extra = 1
+
 class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderInfoInline]
     class Meta:
         model = Order
 
@@ -16,4 +21,3 @@ class TransportationAdmin(admin.ModelAdmin):
 admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderInfo, OrderInfoAdmin)
 admin.site.register(Transportation, TransportationAdmin)
-
