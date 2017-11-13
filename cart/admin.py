1fa8b02ddc23b7af4ecb11ed1d2f657d2f1a033e
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2017-11-13 15:34:40 +0700

    fixes next issue

--- cart/admin.py
+++ cart/admin.py
@@ -1,23 +1,28 @@
 from django.contrib import admin
 from .models import Order, OrderInfo, Transportation
 
+
 class OrderInfoInline(admin.TabularInline):
     model = OrderInfo
     extra = 1
 
+
 class OrderAdmin(admin.ModelAdmin):
     inlines = [OrderInfoInline]
-    
+
     class Meta:
         model = Order
+
 
 class OrderInfoAdmin(admin.ModelAdmin):
     class Meta:
         model = OrderInfo
 
+
 class TransportationAdmin(admin.ModelAdmin):
     class Meta:
         model = Transportation
+
 
 admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderInfo, OrderInfoAdmin)
