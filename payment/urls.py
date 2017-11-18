799945439865bf9ef945120e81dd2af8145cfdf6
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2017-11-18 18:10:14 +0700

    :sparkles: Add destroy creditcard of user

--- payment/urls.py
+++ payment/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^$', CreditCardAction.as_view(), name="payment-creator"),
+    url(r'^$', CreditCardAction.as_view(),
+        name="payment-creator"),
+    url(r'^delete/(?P<token>\w+)/(?P<id>[0-9]+)$', CreditCardDeleteAction.as_view(),
+        name="payment-delete"),
 ]
--- payment/views.py
+++ payment/views.py
@@ -2,8 +2,27 @@ from payment.models import CreditCard
 from payment.serializers import FullCreditCardSerializer
 
 from rest_framework import generics
+from utilities.classes.database import ImpDestroyByTokenView
+from utilities.methods.database import get_customer_by_uid
 
 # Create your views here.
 class CreditCardAction(generics.CreateAPIView):
     queryset = CreditCard.objects.all()
     serializer_class = FullCreditCardSerializer
+
+class CreditCardDeleteAction(ImpDestroyByTokenView):
+    queryset = CreditCard.objects.all()
+    serializer_class = FullCreditCardSerializer
+    credit_id = "credit_card_id"
+
+    def update_kwargs(self, **kwargs):
+        super(CreditCardDeleteAction, self).update_kwargs(**kwargs)
+        self.kwargs[self.credit_id] = kwargs.get('id')
+    
+    def get_object(self):
+        obj = super(CreditCardDeleteAction, self).get_object()
+        cust = get_customer_by_uid(obj.id)
+        return CreditCard.objects.get(
+            id=self.kwargs[self.credit_id],
+            customer=cust
+        )
